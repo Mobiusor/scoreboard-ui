574b448098213f9bc346ef0e44122e2d34df836c
dalingtao <taodaling@gmail.com>
2022-04-04 18:50:52 +0800

    beta color schema

--- src/views/rank.js
+++ src/views/rank.js
@@ -21,24 +21,25 @@ class Rank extends React.Component {
     return this.state.rank.map((x, index) => {
       const badgeColorMapping = {0: 'yellow', 1: 'silver', 2: 'brown'}
 	  const ratingLowerBound = [0, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2700, 3000]
-	  const ratingColorList = ['gray', 'green', 'cyan', 'blue', 'violet', 'orange', 'orange', 'red', 'red', 'black']
+	  const ratingColorList = ['gray', '#8bc34a', '#4caf50', 'cyan', 'blue', 'violet', 'orange', 'orange', 'red', 'red', 'black']
 	  const ratingTitleList = ['Newbie', 'Pupil', 'Apprentice', 'Specialist', 'Expert', 'Candidate Master', 'Master', 'International Master', 'Grandmaster', 'International Grandmaster', 'Legendary Grandmaster']
 	  
-      const badgeColor = badgeColorMapping[index] ?? 'red'
+      
 	  
 	  var ratingIndex = ratingLowerBound.length - 1
 	  while(ratingIndex > 0 && ratingLowerBound[ratingIndex] > x.rating) {
 		  ratingIndex--;
 	  }
-	  
+	  var ratingColor = ratingColorList[ratingIndex]
+	  const badgeColor = badgeColorMapping[index] ?? ratingColor
       return (
-        <div style={{margin: '8px', padding: '8px', borderBottom: '1px solid #ccc'}}>
+        <div style={{margin: '8px', padding: '8px', borderBottom: '1px solid #ccc', background: ratingColor, color: 'white'}}>
           <span style={{marginRight: '32px'}}>
             <Badge count={index + 1} color={badgeColor}>
               <Avatar src={this.state.userMap[x.userId].avatar} />
             </Badge>
           </span>
-          <span style={{ display: 'inline-block', width: '240px', textAlign:'left', marginRight: '32px', color: ratingColorList[ratingIndex] }} > {this.state.userMap[x.userId].name} ({ratingTitleList[ratingIndex] })</span>
+          <span style={{ display: 'inline-block', width: '240px', textAlign:'left', marginRight: '32px'}} > {this.state.userMap[x.userId].name} </span>
           <span style={{ display: 'inline-block', width: '100px', textAlign:'left', marginRight: '32px'}}> 
 		  Score: {x.totalScore} ({x.rating})
 		   </span>
