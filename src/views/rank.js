71d956e28f47028f1b0a63db1fa311f1ad77b094
dalingtao <taodaling@gmail.com>
2022-04-16 10:57:06 +0800

    Update rank.js

--- src/views/rank.js
+++ src/views/rank.js
@@ -20,9 +20,9 @@ class Rank extends React.Component {
   renderItems() {
     return this.state.rank.map((x, index) => {
       const badgeColorMapping = {0: 'yellow', 1: 'silver', 2: 'brown'}
-	  const ratingLowerBound = [0, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2700, 3000]
-	  const ratingColorList = ['gray', '#8bc34a', '#4caf50', 'cyan', 'blue', 'violet', 'orange', 'orange', 'red', 'red', 'black']
-	  const ratingTitleList = ['Newbie', 'Pupil', 'Apprentice', 'Specialist', 'Expert', 'Candidate Master', 'Master', 'International Master', 'Grandmaster', 'International Grandmaster', 'Legendary Grandmaster']
+	  const ratingLowerBound = [0, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2700, 3000, 4000]
+	  const ratingColorList = ['gray', '#8bc34a', '#4caf50', 'cyan', 'blue', 'violet', 'orange', 'orange', 'red', 'red', 'black', '#d9e3f0']
+	  const ratingTitleList = ['Newbie', 'Pupil', 'Apprentice', 'Specialist', 'Expert', 'Candidate Master', 'Master', 'International Master', 'Grandmaster', 'International Grandmaster', 'Legendary Grandmaster', 'God']
 	  
       
 	  
@@ -30,6 +30,7 @@ class Rank extends React.Component {
 	  while(ratingIndex > 0 && ratingLowerBound[ratingIndex] > x.rating) {
 		  ratingIndex--;
 	  }
+	  //ratingIndex = index;
 	  var ratingColor = ratingColorList[ratingIndex]
 	  const badgeColor = badgeColorMapping[index] ?? ratingColor
       return (
@@ -39,12 +40,12 @@ class Rank extends React.Component {
               <Avatar src={this.state.userMap[x.userId].avatar} />
             </Badge>
           </span>
-          <span style={{ display: 'inline-block', width: '240px', textAlign:'left', marginRight: '32px'}} > {this.state.userMap[x.userId].name} </span>
-          <span style={{ marginRight: '32px' }}> 
-		  Score: {x.totalScore}
+          <span style={{ display: 'inline-block', width: '140px', textAlign:'left', marginRight: '32px'}} > {this.state.userMap[x.userId].name} </span>
+          <span style={{ display: 'inline-block',textAlign:'left', marginRight: '32px', width: '160px'}}> 
+			  {ratingTitleList[ratingIndex]}
 		   </span>
-		   <span> 
-		  Rating: {x.rating}({x.trueSkill})
+		  <span style={{ display: 'inline-block',textAlign:'left', marginRight: '32px'}}> 
+			  {x.rating}
 		   </span>
         </div>
       )
